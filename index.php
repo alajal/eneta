9ffe506dd0511d2b0645f11b08192870aec84a2e
valgma <lembit.valgma@gmail.com>
2015-02-23 21:14:18 +0200

    indeks.php

--- index.php
+++ index.php
@@ -1,8 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Eneta</title>
+    <link rel="stylesheet" href="css/bootstrap.css"/>
+    <link rel="stylesheet" href="css/style.css"/>
+    <script src="js/main.js"></script>
+</head>
+<body>
+    <header>
+
+        <nav class="top">
+            <p>Teretulemast!</p>
+            <p class="logo">
+            </p>
+
+            <div class="right">
+                <ul>
+                    <li>Liige?</li>
+                    <li class="login">
+                        <a href="/pages/login/">Register / Login</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        <nav class="nav">
+            <div class="menu-wrapper">
+                <ul class="menu">
+                    <li class="uudised">
+                        <a href="/pages/uudised/">Uudised</a>
+                    </li>
+
+                    <li class="oppimiseks">
+                        <a href="/pages/oppimiseks/">Õppimiseks</a>
+                    </li>
+
+                    <li class="grupid">
+                        <a href="/pages/grupid/">Grupid</a>
+                    </li>
+
+                    <li class="yritused">
+                        <a href="/pages/yritused/">Üritused</a>
+                    </li>
+
+                    <li class="blogi">
+                        <a href="/pages/yritused/">Blogi</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+    </header>
+    <br>
+    <div class="content">
+        <div class="news">
+            <!--koht, kus javascript loob uued uudised-->
+            <p>Sisu</p>
+        </div>
+    </div>
+
+    <footer>
+        <div class="footer">
+            <p id="kontakt">Kontakt</p>
+            <p id="enetast">Enetast lähemalt</p>
+        </div>
+    </footer>
+</body>
+</html>
+
 <!--
-
-/*/
-echo "Hello World";
-echo "\n";
-*/
-
--->
+// /etc/init.d/apache2 start
+// /etc/apache2 on konf ls-l-->
