e0d81ea0bf3d3838dd3c930835048fc58c1c49eb
valgma <lembit.valgma@gmail.com>
2015-03-07 16:57:26 +0200

    lihtsustasin "content" osa loogikat - yldiselt on ikka sisu pikem kui sidebar lisasin natuke javascripti (jquery), mis kuvab/peidab uudiseid/sisestamist lisasin lokaalse jquery faili

--- grupid.php
+++ grupid.php
@@ -5,7 +5,7 @@
     <title>Eneta</title>
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/style.css"/>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="js/jquery-2.1.3.js"></script>
     <script src="js/main.js"></script>
 </head>
 <body>
@@ -15,19 +15,16 @@ include('templates/header_template.php')
 ?>
 
     <div id="content">
-        <div id="content-container">
-            <div id="left-sidebar">
-                <ul>
-                    <li><a href="#">Grupp 1</a> </li>
-                    <li><a href="#">Grupp 2</a> </li>
-                </ul>
-            </div>
+        <div id="left-sidebar">
+            <ul>
+                <li><a href="#">Grupp 1</a> </li>
+                <li><a href="#">Grupp 2</a> </li>
+            </ul>
+        </div>
 
-            <div id="first-content-col">
+        <div id="content-col-1" class="content-col">
 
-                <p>Sisu</p>
-
-            </div>
+            <p>Sisu</p>
 
         </div>
     </div>
--- templates/index_template.php
+++ templates/index_template.php
@@ -5,7 +5,7 @@
     <title>Eneta</title>
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/style.css"/>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="js/jquery-2.1.3.js"></script>
     <script src="js/main.js"></script>
 </head>
 <body>
@@ -15,29 +15,30 @@ include('header_template.php')
 ?>
 
 <div id="content">
-    <div id="content-container">
-        <div id="left-sidebar">
-            <ul>
-                <li><a href="#">Kuva uudised</a> </li>
-                <li><a href="#">Sisesta uudised</a> </li>
-            </ul>
-        </div>
+    <div id="left-sidebar">
+        <ul>
+            <li><a href="#" id="show-news">Kuva uudised</a> </li>
+            <li><a href="#" id="show-news-input">Sisesta uudised</a> </li>
+        </ul>
+    </div>
 
-        <div id="first-content-col">
-            <!--koht, kus javascript loob uued uudised-->
-            <p>Sisu</p>
+    <div id="content-col-1" class="content-col">
+        <!--koht, kus javascript loob uued uudised-->
+        <p>Sisu</p>
 
-            <?php
-            if(count($messages) > 0) {
-                foreach($messages as $message) {
-                    echo "<p>".$message["message"]."</p>";
-                }
-            } else {
-                echo "<p>mingi jama</p>";
+        <?php
+        if(count($messages) > 0) {
+            foreach($messages as $message) {
+                echo "<p>".$message["message"]."</p>";
             }
-            ?>
-        </div>
+        } else {
+            echo "<p>mingi jama</p>";
+        }
+        ?>
+    </div>
 
+    <div id="content-col-2" class="not content-col">
+        <p>Siia tuleks the sisestamine</p>
     </div>
 </div>
 
