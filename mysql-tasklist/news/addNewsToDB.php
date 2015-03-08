9259022b4ba02744b5a048174f790496c11aaf81
valgma <lembit.valgma@gmail.com>
2015-03-08 16:18:30 +0200

    a

--- mysql-tasklist/news/addNewsToDB.php
+++ mysql-tasklist/news/addNewsToDB.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once 'functions.php';
+include_once 'mysql-tasklist/news/functions.php';
 
 $news_user = "unknown";
 $news_title = $_POST['input-news-title'];
@@ -8,5 +8,5 @@ $news_content = $_POST['input-news-content'];
 $news_date = date("Y-m-d H:i:s");
 addNews($news_user, $news_title, $news_content, $news_date);
 
-header('Location: ../../index.php');
+header('Location: index.php');
 ?>
