be7382d1cb1d4d42c2e6dec06929de4bd37dcd13
valgma <lembit.valgma@gmail.com>
2015-03-08 16:25:03 +0200

    sad

--- templates/sisesta_template.php
+++ templates/sisesta_template.php
@@ -1,5 +1,5 @@
 
-<form action="mysql-tasklist/news/addNewsToDB.php" method="post">
+<form action="/mysql-tasklist/news/addNewsToDB.php" method="post">
     <p>Pealkiri</p>
     <textarea type="text" name="input-news-title" maxlength="255" rows="1" cols="50"></textarea>
     <p>Sisu</p>
