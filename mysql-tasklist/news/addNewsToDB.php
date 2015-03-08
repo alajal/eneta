8508bd17d7292959ea45cd9e5732ef5fba098340
valgma <lembit.valgma@gmail.com>
2015-03-08 17:22:20 +0200

    see töötas lokaalselt..vaatame kas ka azures

--- mysql-tasklist/news/addNewsToDB.php
+++ mysql-tasklist/news/addNewsToDB.php
@@ -8,5 +8,5 @@ $news_content = $_POST['input-news-content'];
 $news_date = date("Y-m-d H:i:s");
 addNews($news_user, $news_title, $news_content, $news_date);
 
-header('Location: /../../index.php');
+header('Location: ../../index.php');
 ?>
