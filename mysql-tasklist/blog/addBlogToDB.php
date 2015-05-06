85d22e55ed1087ee43a4a3745b10c5f6e6a6ca91
valgma <lembit.valgma@gmail.com>
2015-05-06 14:53:09 +0300

    kohalikus serveris töötab...proovib veel

--- mysql-tasklist/blog/addBlogToDB.php
+++ mysql-tasklist/blog/addBlogToDB.php
@@ -9,7 +9,12 @@ if(isUserLoggedIn()){   //kontroll - ainult sisselogitud kasutaja saab uudiseid 
     //$blog_user = $_POST['blog-user'];
     $blog_name = $_POST['blog-name'];
     addBlog($blog_user, $blog_name);
+} else {
+    $blog_user = "oskar.ohakas@ut.ee";
+    //$blog_user = $_POST['blog-user'];
+    $blog_name = $_POST['blog-name'];
+    addBlog($blog_user, $blog_name);
 }
 
-header('Location: /index.php');
+header('Location: ../../index.php');
 ?>
