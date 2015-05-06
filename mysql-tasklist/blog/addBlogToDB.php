a9b2e5ef02906bfc437d79619ae7e98fb3a800f1
valgma <lembit.valgma@gmail.com>
2015-05-06 13:56:52 +0300

    ei saanud korda username automaatselt - teeme manuaalselt

--- mysql-tasklist/blog/addBlogToDB.php
+++ mysql-tasklist/blog/addBlogToDB.php
@@ -4,8 +4,9 @@ include_once (__DIR__."../news/functions.php");
 include_once ("../../session.php");
 
 if(isUserLoggedIn()){   //kontroll - ainult sisselogitud kasutaja saab uudiseid lisada
-    $blog_user = getLoggedInUserEmail();
-    $blog_name = $_POST['blogname'];
+    //$blog_user = getLoggedInUserEmail();
+    $blog_user = $_POST['blog-user'];
+    $blog_name = $_POST['blog-name'];
     addBlog($blog_user, $blog_name);
 }
 
--- templates/profile_template.php
+++ templates/profile_template.php
@@ -7,8 +7,24 @@ echo $bla;
 ?>
 
 <form action='/mysql-tasklist/blog/addBlogToDB.php' method='post' id='edit-news-form-id'>
-    <label for='blogname-input'>Blogi nimi: </label>
-    <input type='text' name='blogname' id='blogname-input' maxlength='45'>
+    <label for='blog-name-id'>Blogi nimi: </label>
+    <input type='text' name='blog-name' id='blog-name-id' maxlength='45'>
+
+    <br>
+    <label for='blog-user-id'>kasutaja: </label>
+    <select name='blog-user' id='blog-user-id'>
+        <?php
+        include '../mysql-tasklist/news/functions.php';
+        $users = getUsers();
+        if(count($users) > 0) {
+            foreach($users as $user) {
+                echo "<option value='".$user["mail"]."'>".$user["lastname"]."</option>";
+            }
+        } else {
+            echo "<p>mingi jama</p>";
+        } ?>
+
+    </select>
 
     <input type='submit' name='edit-submit-news''>
 
