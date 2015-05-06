df75df083b6e5471ddea418db8e7746ba54352b7
valgma <lembit.valgma@gmail.com>
2015-05-06 14:15:42 +0300

    mingi probleem..proovime

--- templates/profile_template.php
+++ templates/profile_template.php
@@ -6,7 +6,7 @@ $bla = $_SESSION['googleuserid'];
 echo $bla;
 ?>
 
-<form action='/mysql-tasklist/blog/addBlogToDB.php' method='post' id='add-blog-form-id'>
+<form action='mysql-tasklist/blog/addBlogToDB.php' method='post' id='add-blog-form-id'>
     <label for='blog-name-id'>Blogi nimi: </label>
     <input type='text' name='blog-name' id='blog-name-id' maxlength='45'>
 
