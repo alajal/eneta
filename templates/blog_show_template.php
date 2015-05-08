4819de20fc5b4b16da9a8e0ed724d275d5245c2b
valgma <lembit.valgma@gmail.com>
2015-05-08 15:01:40 +0300

    töötab!
    kuvab esimest blogi vaikimisi
    tsipa blogi css'i

--- templates/blog_show_template.php
+++ templates/blog_show_template.php
@@ -2,6 +2,16 @@
 include (__DIR__."/../mysql-tasklist/news/functions.php");
 
 $blogname = $_GET["blogname"];
+
+if ($blogname == "") {
+    $blogs = getBlognames();
+    foreach ($blogs as $blog) {
+        $blogname = $blog["blogname"];
+        break;
+    }
+}
+
 $blogentries = getBlogEntriesByName($blogname);
 
+echo "<h3 class='blog-title'>$blogname</h3>";
 echo getBlogEntryHtml($blogentries);
