f5300110d6acdf44e81c7973e713c1113f0b00e6
valgma <lembit.valgma@gmail.com>
2015-05-11 18:55:42 +0300

    test

--- templates/profile_template.php
+++ templates/profile_template.php
@@ -3,12 +3,14 @@ if(isUserLoggedIn()) {
     $username = getLoggedInUserEmail();
     $display_name = getUsersByUsername($username);
     $display_name = $display_name["firstname"]." ".$display_name["lastname"];
+    $google_full_name = getLoggedInUserFullName();
     echo "
     <h3 id='profile-display-message'>Profiil</h3>
     <span id='profile-user-name' class='not'>$username</span>
     <p>Kasutajanimi: $username<br>Nimi: <span id='profile-display-name'>$display_name</span>
     <button id='profile-edit-name-enable'>Muuda nimi</button>
     </p>
+    <span id='profile-user-full-name'>$google_full_name</span>
     <form action='mysql-tasklist/news/updateUserInDB.php' method='post' id='update-user-form-id' class='not'>
         <label for='update-user-first-name-id'>Eesnimi: </label>
         <input type='text' name='update-user-first-name' id='update-user-first-name-id' maxlength='45'>
